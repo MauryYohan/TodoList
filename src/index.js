a4e06daaf9d3cebb3feb52f286e6da02e0363c2d
Yohan Maury <mauryyohan1@gmail.com>
2021-04-23 20:16:50 +0200

    Edit is now possible for all TODO's

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ const input = document.querySelector('form > input');
 const todos = [
     {
         text: 'Je suis une todo',
-        done: false
+        done: false,
+        editMode: false
     }, 
     {
         text: 'faire du JS',
-        done: true
+        done: true,
+        editMode: false
     }
 ]
 
@@ -21,9 +23,8 @@ form.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
     let value = input.value;
-    if (!value) {
+    if (!value)
         value = "Todo par defaut";
-    }
     input.value = '';
     addTodo(value);
 })
@@ -31,7 +32,10 @@ form.addEventListener('submit', (evt) => {
 // FONCTIONS
 const displayTodo = () => {
     const todosNode = todos.map((todo, index) => {
-        return createTodoElement(todo, index);
+        if (todo.editMode)
+            return createTodoEditElement(todo, index);
+        else
+            return createTodoElement(todo, index);
     })
     ul.innerHTML = '';
     ul.append(...todosNode);
@@ -40,15 +44,53 @@ const displayTodo = () => {
 const addTodo = (text) => {
     todos.push({
         text,
-        done: false
+        done: false,
+        editMode: true
     });
     displayTodo();
 }
 
+const createTodoEditElement = (todo, index) => {
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    const btnCancel = document.createElement('button');
+    const btnSave = document.createElement('button');
+
+    input.type = 'text';
+    input.value = todo.text;
+
+    btnSave.innerHTML = "Save";
+    btnCancel.innerHTML = "Cancel";
+
+    btnSave.addEventListener("click", (evt) => {
+        evt.stopPropagation();
+        editTodo(index, input);
+    })
+
+    btnCancel.addEventListener('click', (evt) => {
+        evt.stopPropagation();
+        toggleEditMode(index);
+        // Function editTodo()
+    });
+
+    li.append(input, btnSave, btnCancel);
+    return li;
+}
+
 const createTodoElement = (todo, index) => {
     const li = document.createElement('li');
+    const editBtn = document.createElement('button');
     const delBtn = document.createElement('button');
+    editBtn.innerHTML = "Editer";
     delBtn.innerHTML = "Supprimer";
+
+    editBtn.addEventListener('click', (evt) => {
+        evt.stopPropagation();
+        console.log("dfgrfhtgh");
+        toggleEditMode(index);
+        // Function editTodo()
+    });
+    
     delBtn.addEventListener('click', (evt) => {
         evt.stopPropagation();
         delTodo(index);
@@ -57,15 +99,29 @@ const createTodoElement = (todo, index) => {
     <span class="todo ${ todo.done ? 'done' : '' }"></span>
     <p>${ todo.text }</p>
     `;
+    // Changer ce <li> 
     li.addEventListener('click', (evt) => {
         toggleTodo(index);
     })
+    li.appendChild(editBtn);
     li.appendChild(delBtn);
     return li;
 }
 
 const toggleTodo  = (index) => {
     todos[index].done = !todos[index].done;
+    displayTodo();
+}
+
+const toggleEditMode = (index) => {
+    todos[index].editMode = !todos[index].editMode;
+    displayTodo();
+}
+
+const editTodo = (index, input) => {
+    const value = input.value;
+    todos[index].text = value;
+    toggleEditMode(index);
     displayTodo();
 }
 
